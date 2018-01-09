17b666338f82c1ec19ad4d32c51f43e2e3ba6d66
OnuPete <pcr137@case.edu>
2018-01-08 16:51:30 -0800

    added basic styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,34 +13,20 @@
         <!-- Styles -->
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
             .position-ref {
                 position: relative;
             }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
             .content {
                 text-align: center;
+                color: #FEFEFE;
             }
 
         </style>
     </head>
     <body>
-      <h2 class="position-ref">Doggy Search</h2>
-      <div class="content" id="root"></div>
+      <h2 class="position-ref content">Doggy Search</h2>
+      <div id="root"></div>
       <script src="{{mix('js/app.js')}}" ></script>
     </body>
 </html>
